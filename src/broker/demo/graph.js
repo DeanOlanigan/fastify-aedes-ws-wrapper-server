89f2f6f6552f6183efb4d59c31bc539fda1b49e7
DeanOLanigan <145742378+DeanOlanigan@users.noreply.github.com>
2025-10-07 17:14:54 +0700

    graph 1

--- src/broker/demo/graph.js
+++ src/broker/demo/graph.js
@@ -1,9 +1,16 @@
 export async function graph(broker) {
     const msg = {
         ts: Date.now(),
-        name: "Demo name",
+        name: "IRZGPIO_BattLow",
         desc: "Demo description",
         value: Math.random() * 100,
         measurement: "V",
     };
+
+    broker.publish({
+        topic: "graph/IRZGPIO_BattLow",
+        payload: JSON.stringify(msg),
+        qos: 0,
+        retain: false,
+    });
 }
