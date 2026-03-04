09521a4fb7e424a4d80204c6e4ca4764a433dfc9
DeanOlanigan <145742378+DeanOlanigan@users.noreply.github.com>
2026-03-05 00:11:29 +0700

    cors put fix

--- src/http/server.js
+++ src/http/server.js
@@ -35,7 +35,11 @@ export async function createHttpServer({ logLevel }) {
         rolling: true,
         saveUninitialized: false,
     })
-    await fastify.register(cors, { origin: "http://localhost:5173", credentials: true });
+    await fastify.register(cors, {
+        origin: "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+    });
     await fastify.register(rateLimit, { max: 200, timeWindow: "1 minute" });
     await fastify.register(fastifyMultipart, {
         limits: {
